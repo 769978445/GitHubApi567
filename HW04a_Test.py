a930703a534e22eedf7dbb62e76b916bc56e5133
769978445 <57587953+769978445@users.noreply.github.com>
2020-02-18 05:05:48 -0500

    Update HW04a_Test.py

--- HW04a_Test.py
+++ HW04a_Test.py
@@ -1,20 +1,21 @@
 import unittest
+
 from HW04a import get_repo_info
 
 
-class TestOutput(unittest.TestCase):
-    def test_result(self):
-        expect_result = ['User: 769978445',
-                         'Repo: 567HW2 Number of commits: 8',
-                         'Repo: 810Project Number of commits: 1',
-                         'Repo: GitHubApi567 Number of commits: 3',
-                         'Repo: MGR4Python Number of commits: 1',
-                         'Repo: SSW567 Number of commits: 2', ]
-        self.assertEqual(get_repo_info(), expect_result)
+class TestGetRepo(unittest.TestCase):
+    def test_normal_response(self):
+        expected = ['User: 769978445',
+                    'Repo: 567HW2 Number of commits: 8',
+                    'Repo: 810Project Number of commits: 1',
+                    'Repo: GitHubApi567 Number of commits: 10',
+                    'Repo: GitHubApi567-1 Number of commits: 5',
+                    'Repo: MGR4Python Number of commits: 1',
+                    'Repo: SSW567 Number of commits: 2']
+        self.assertEqual(get_repo_info(), expected)
 
-    def test_terrible_user_name(self):
-        self.assertEqual(get_repo_info('123456'), 'unable to fetch repos from user')
-        self.assertEqual(get_repo_info('XiangyuWang'), 'unable to fetch repos from user')
+    def test_bad_user_name(self):
+        self.assertEqual(get_repo_info('aaaabbbbcccc'), 'unable to fetch repos from user')
         self.assertEqual(get_repo_info(''), 'unable to fetch repos from user')
 
 
